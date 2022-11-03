03f5df1adb976c25dac2053735989dfe57af62ad
yujin9747 <jyj9747@naver.com>
2022-11-03 16:49:08 +0900

    A-B

--- AminusB/src/aMinusb/AminusB.java
+++ AminusB/src/aMinusb/AminusB.java
@@ -1,3 +1,12 @@
+/**
+ * A-B
+ * 1001번 문제
+ * Stage 1 - 3
+ * 
+ * https://www.acmicpc.net/problem/1001
+ * 
+ * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ */
 package aMinusb;
 import java.util.Scanner;
 
