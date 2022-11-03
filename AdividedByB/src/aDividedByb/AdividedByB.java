ec807565d19ed2c7732dc5290c16158bb5f51a85
yujin9747 <jyj9747@naver.com>
2022-11-03 17:00:30 +0900

    A/B

--- AdividedByB/src/aDividedByb/AdividedByB.java
+++ AdividedByB/src/aDividedByb/AdividedByB.java
@@ -1,3 +1,12 @@
+/**
+ * A/B
+ * 1008번 문제
+ * Stage 1 - 5
+ * 
+ * https://www.acmicpc.net/problem/1008
+ * 
+ * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ * */
 package aDividedByb;
 import java.util.Scanner;
 
