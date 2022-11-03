334f6279cf0359ebe66e910cd45a6e8e56f46f05
yujin9747 <jyj9747@naver.com>
2022-11-03 16:20:00 +0900

    Hello Wolrd 출

---
+++ HelloWorld.java/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Hello World
+ * 2557번 문제
+ * Stage1 - 1
+ * 
+ * https://www.acmicpc.net/problem/2557
+ * */
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
