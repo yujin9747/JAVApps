7bbebbc30eb0f87a8c2d15d2ec93fee5e4b940b8
yujin9747 <jyj9747@naver.com>
2022-11-03 17:12:38 +0900

    Print

---
+++ Print/src/print/main.java
@@ -0,0 +1,25 @@
+/**
+ * ??!
+ * 10926번 문제
+ * Stage 1 - 7
+ * 
+ * https://www.acmicpc.net/problem/10926
+ * 
+ * 입력: 이름
+ * 출력: 이름??!
+ * */
+package print;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String name = in.next();
+		in.close();
+		System.out.printf("%s??!", name);
+
+	}
+
+}
